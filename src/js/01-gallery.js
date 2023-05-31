309f269ae09a3748826c5aa6c9fb6b6e02caf22d
Yuliia Hubarenko <xlapushka@ukr.net>
2023-05-31 12:01:59 +0300

    try again

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+(function () {
+  const markup = galleryItems
+    .map(
+      ({ preview, original, description }) => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            src="${preview}"
+            alt="${description}"
+            width="100%"
+            height=auto
+            class="gallery__image"
+          />
+        </a>  
+      </li>`
+    )
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', markup);
+})();
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: '250',
+});
